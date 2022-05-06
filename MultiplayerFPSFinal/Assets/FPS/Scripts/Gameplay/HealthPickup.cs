371fbe3d439a3a95299212b7cd418e5e8eec4023
AIE\s213321 <s213321@students.aie.edu.au>
2022-05-06 15:35:14 +0930

    Lighting and Fog fixed. Adjusted colours. added enemy placement in far areas

--- MultiplayerFPSFinal/Assets/FPS/Scripts/Gameplay/HealthPickup.cs
+++ MultiplayerFPSFinal/Assets/FPS/Scripts/Gameplay/HealthPickup.cs
@@ -1,19 +1,22 @@
 ﻿using Unity.FPS.Game;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Unity.FPS.Gameplay
 {
+    
     public class HealthPickup : Pickup
     {
         [Header("Parameters")] [Tooltip("Amount of health to heal on pickup")]
         public float HealAmount;
-
+        public UnityEvent action;
         protected override void OnPicked(PlayerCharacterController player)
         {
             Health playerHealth = player.GetComponent<Health>();
             if (playerHealth && playerHealth.CanPickup())
             {
                 playerHealth.Heal(HealAmount);
+                action.Invoke();
                 PlayPickupFeedback();
                 Destroy(gameObject);
             }
